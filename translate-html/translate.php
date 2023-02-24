2e2a66a2cfe3b8be7a9e220c560f0bf08a432ecc
Cinemato <alharerekhaled@gmail.com>
2023-02-24 16:47:10 +0300

    Created translation PHP script with Google API

---
+++ translate-html/translate.php
@@ -0,0 +1,168 @@
+
+<?php
+// Put HTML here to translate
+$html = '
+<nav class="row wrap xlarge-up-nowrap margin-bottom-medium">
+      <div class="row width-100 medium-up-width-4-5 xlarge-up-width-1-2">
+        <div class="width-100 large-up-width-1-3 border-box padding-right-medium margin-bottom-medium">
+          <a href="../../subjects" class="inline-block margin-bottom-xxsmall text-2 color-charcoal weight-bold border-bottom border-gray hover-no-underline">Browse by subject</a>
+          <ul class="list-no-style text-2">
+                          <li class="margin-vert-xxsmall">
+                <a class="color-charcoal" href="../../subject/cs">Computer Science</a>
+              </li>
+                          <li class="margin-vert-xxsmall">
+                <a class="color-charcoal" href="../../subject/psychology">Psychology</a>
+              </li>
+                          <li class="margin-vert-xxsmall">
+                <a class="color-charcoal" href="../../subject/cybersecurity">Cybersecurity</a>
+              </li>
+                          <li class="margin-vert-xxsmall">
+                <a class="color-charcoal" href="../../subject/health">Health</a>
+              </li>
+                          <li class="margin-vert-xxsmall">
+                <a class="color-charcoal" href="../../subject/law">Law</a>
+              </li>
+                          <li class="margin-vert-xxsmall">
+                <a class="color-charcoal" href="../../subject/accounting">Accounting</a>
+              </li>
+                          <li class="margin-vert-xxsmall">
+                <a class="color-charcoal" href="https://www.classcentral.com/subject/web-development">Web Development</a>
+              </li>
+                      </ul>
+        </div>
+        <div class="width-100 large-up-width-1-3 border-box padding-right-medium margin-bottom-medium">
+          <a href="../../providers" class="inline-block margin-bottom-xxsmall text-2 color-charcoal weight-bold border-bottom border-gray hover-no-underline">Browse by provider</a>
+          <ul class="list-no-style text-2">
+                          <li class="margin-vert-xxsmall">
+                <a class="color-charcoal" href="../../provider/coursera">Coursera</a>
+              </li>
+                          <li class="margin-vert-xxsmall">
+                <a class="color-charcoal" href="../../provider/edx">edX</a>
+              </li>
+                          <li class="margin-vert-xxsmall">
+                <a class="color-charcoal" href="../../provider/futurelearn">FutureLearn</a>
+              </li>
+                          <li class="margin-vert-xxsmall">
+                <a class="color-charcoal" href="../../provider/udacity">Udacity</a>
+              </li>
+                          <li class="margin-vert-xxsmall">
+                <a class="color-charcoal" href="https://www.classcentral.com/provider/swayam">Swayam</a>
+              </li>
+                          <li class="margin-vert-xxsmall">
+                <a class="color-charcoal" href="../../provider/udemy">Udemy</a>
+              </li>
+                          <li class="margin-vert-xxsmall">
+                <a class="color-charcoal" href="../../provider/linkedin-learning">LinkedIn Learning</a>
+              </li>
+                      </ul>
+        </div>
+        <div class="width-100 large-up-width-1-3 border-box padding-right-medium margin-bottom-medium">
+          <a href="../../universities" class="inline-block text-2 margin-bottom-xsmall color-charcoal weight-bold border-bottom border-gray hover-no-underline">Browse by university</a>
+          <ul class="list-no-style text-2">
+                          <li class="margin-vert-xxsmall">
+                <a class="color-charcoal" href="../../university/harvard">Harvard</a>
+              </li>
+                          <li class="margin-vert-xxsmall">
+                <a class="color-charcoal" href="../../university/stanford">Stanford</a>
+              </li>
+                          <li class="margin-vert-xxsmall">
+                <a class="color-charcoal" href="../../university/gatech">Georgia Tech</a>
+              </li>
+                          <li class="margin-vert-xxsmall">
+                <a class="color-charcoal" href="../../university/umich">University of Michigan</a>
+              </li>
+                          <li class="margin-vert-xxsmall">
+                <a class="color-charcoal" href="../../university/purdue">Purdue University</a>
+              </li>
+                          <li class="margin-vert-xxsmall">
+                <a class="color-charcoal" href="../../university/duke">Duke University</a>
+              </li>
+                          <li class="margin-vert-xxsmall">
+                <a class="color-charcoal" href="../../university/iitm">IIT Madras</a>
+              </li>
+                      </ul> 
+        </div>
+      </div>
+      <div class="row width-100 medium-up-width-3-5 xlarge-up-width-1-2">
+        <div class="width-100 large-up-width-2-5 border-box padding-right-medium margin-bottom-medium">
+          <a href="https://www.classcentral.com/institutions" class="inline-block margin-bottom-xxsmall text-2 color-charcoal weight-bold border-bottom border-gray hover-no-underline">Browse by institution</a>
+          <ul class="list-no-style text-2">
+                          <li class="margin-vert-xxsmall">
+                <a class="color-charcoal" href="../../institution/google">Google</a>
+              </li>
+                            <li class="margin-vert-xxsmall">
+                <a class="color-charcoal" href="../../institution/microsoft">Microsoft</a>
+              </li>
+                            <li class="margin-vert-xxsmall">
+                <a class="color-charcoal" href="../../institution/ibm">IBM</a>
+              </li>
+                            <li class="margin-vert-xxsmall">
+                <a class="color-charcoal" href="../../institution/amazon">Amazon</a>
+              </li>
+                            <li class="margin-vert-xxsmall">
+                <a class="color-charcoal" href="../../institution/linuxfoundation">Linux Foundation</a>
+              </li>
+                            <li class="margin-vert-xxsmall">
+                <a class="color-charcoal" href="../../institution/british-council">British Council</a>
+              </li>
+                            <li class="margin-vert-xxsmall">
+                <a class="color-charcoal" href="../../institution/salesforce">Salesforce</a>
+              </li>
+                        </ul>
+        </div>
+        <div class="width-100 large-up-width-3-5 margin-bottom-small">
+          <a href="../../rankings" class="inline-block margin-bottom-xxsmall text-2 color-charcoal weight-bold border-bottom border-gray hover-no-underline">Rankings</a>
+          <ul class="list-no-style text-2">
+                          <li class="margin-vert-xxsmall">
+                <a class="color-charcoal" href="../../collection/top-free-online-courses">Best Online Courses of All Time</a>
+              </li>
+                          <li class="margin-vert-xxsmall">
+                <a class="color-charcoal" href="../best-free-online-courses-2022/index.html">Best Online Courses of the Year (2022 Edition)</a>
+              </li>
+                          <li class="margin-vert-xxsmall">
+                <a class="color-charcoal" href="../most-popular-online-courses/index.html">Most Popular Courses of All Time</a>
+              </li>
+                          <li class="margin-vert-xxsmall">
+                <a class="color-charcoal" href="../most-popular-courses-2022/index.html">Most Popular Courses of the Year (2022 Edition)</a>
+              </li>
+                          <li class="margin-vert-xxsmall">
+                <a class="color-charcoal" href="../coursera-top-courses/index.html">250 Top FREE Coursera Courses of All Time</a>
+              </li>
+                          <li class="margin-vert-xxsmall">
+                <a class="color-charcoal" href="../edx-top-courses/index.html">100 Top FREE edX Courses of All Time</a>
+              </li>
+                          <li class="margin-vert-xxsmall">
+                <a class="color-charcoal" href="../udemy-top-courses/index.html">250 Top Udemy Courses of All Time</a>
+              </li>
+                      </ul>
+        </div>
+      </div>
+    </nav>
+'; 
+
+// Require Google Translate
+require_once ('vendor/autoload.php');
+use Stichoza\GoogleTranslate\GoogleTranslate;
+
+// Create Google Translate object
+$tr = new GoogleTranslate();
+
+// Set the translation "from" and "to"
+$tr->setSource('en'); // Translate from English
+$tr->setTarget('hi'); // Translate to Hindi
+
+// Fetch all the text nodes and translate
+libxml_use_internal_errors(true);
+$dom = new DomDocument();
+$dom->loadHTML($html, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+$xpath = new DOMXPath($dom);
+
+foreach ($xpath->query('//text()') as $text) {
+    if (trim($text->nodeValue)) {
+        $text->nodeValue = $tr->translate($text->nodeValue);
+    }
+}
+
+// Save the translated file
+echo $dom->saveHTMLFile('translated.html');
+?>
---
+++ translate-html/vendor/composer/InstalledVersions.php
@@ -0,0 +1,321 @@
+<?php
+
+
+
+
+
+
+
+
+
+
+
+namespace Composer;
+
+use Composer\Semver\VersionParser;
+
+
+
+
+
+
+class InstalledVersions
+{
+private static $installed = array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'guzzlehttp/guzzle' => 
+    array (
+      'pretty_version' => '7.5.0',
+      'version' => '7.5.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b50a2a1251152e43f6a37f0fa053e730a67d25ba',
+    ),
+    'guzzlehttp/promises' => 
+    array (
+      'pretty_version' => '1.5.2',
+      'version' => '1.5.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b94b2807d85443f9719887892882d0329d1e2598',
+    ),
+    'guzzlehttp/psr7' => 
+    array (
+      'pretty_version' => '2.4.3',
+      'version' => '2.4.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '67c26b443f348a51926030c83481b85718457d3d',
+    ),
+    'psr/http-client' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2dfb5f6c5eff0e91e20e913f8c5452ed95b86621',
+    ),
+    'psr/http-client-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'psr/http-factory' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '12ac7fcd07e5b077433f5f2bee95b3a771bf61be',
+    ),
+    'psr/http-factory-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'psr/http-message' => 
+    array (
+      'pretty_version' => '1.0.1',
+      'version' => '1.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f6561bf28d520154e4b0ec72be95418abe6d9363',
+    ),
+    'psr/http-message-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'ralouphie/getallheaders' => 
+    array (
+      'pretty_version' => '3.0.3',
+      'version' => '3.0.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '120b605dfeb996808c31b6477290a714d356e822',
+    ),
+    'stichoza/google-translate-php' => 
+    array (
+      'pretty_version' => 'v4.1.7',
+      'version' => '4.1.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '14f6a9c84cd860910706ee3a668fba8a0a1aac0f',
+    ),
+    'symfony/deprecation-contracts' => 
+    array (
+      'pretty_version' => 'v2.5.2',
+      'version' => '2.5.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e8b495ea28c1d97b5e0c121748d6f9b53d075c66',
+    ),
+  ),
+);
+
+
+
+
+
+
+
+public static function getInstalledPackages()
+{
+return array_keys(self::$installed['versions']);
+}
+
+
+
+
+
+
+
+
+
+public static function isInstalled($packageName)
+{
+return isset(self::$installed['versions'][$packageName]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public static function satisfies(VersionParser $parser, $packageName, $constraint)
+{
+$constraint = $parser->parseConstraints($constraint);
+$provided = $parser->parseConstraints(self::getVersionRanges($packageName));
+
+return $provided->matches($constraint);
+}
+
+
+
+
+
+
+
+
+
+
+public static function getVersionRanges($packageName)
+{
+if (!isset(self::$installed['versions'][$packageName])) {
+throw new \OutOfBoundsException('Package "' . $packageName . '" is not installed');
+}
+
+$ranges = array();
+if (isset(self::$installed['versions'][$packageName]['pretty_version'])) {
+$ranges[] = self::$installed['versions'][$packageName]['pretty_version'];
+}
+if (array_key_exists('aliases', self::$installed['versions'][$packageName])) {
+$ranges = array_merge($ranges, self::$installed['versions'][$packageName]['aliases']);
+}
+if (array_key_exists('replaced', self::$installed['versions'][$packageName])) {
+$ranges = array_merge($ranges, self::$installed['versions'][$packageName]['replaced']);
+}
+if (array_key_exists('provided', self::$installed['versions'][$packageName])) {
+$ranges = array_merge($ranges, self::$installed['versions'][$packageName]['provided']);
+}
+
+return implode(' || ', $ranges);
+}
+
+
+
+
+
+public static function getVersion($packageName)
+{
+if (!isset(self::$installed['versions'][$packageName])) {
+throw new \OutOfBoundsException('Package "' . $packageName . '" is not installed');
+}
+
+if (!isset(self::$installed['versions'][$packageName]['version'])) {
+return null;
+}
+
+return self::$installed['versions'][$packageName]['version'];
+}
+
+
+
+
+
+public static function getPrettyVersion($packageName)
+{
+if (!isset(self::$installed['versions'][$packageName])) {
+throw new \OutOfBoundsException('Package "' . $packageName . '" is not installed');
+}
+
+if (!isset(self::$installed['versions'][$packageName]['pretty_version'])) {
+return null;
+}
+
+return self::$installed['versions'][$packageName]['pretty_version'];
+}
+
+
+
+
+
+public static function getReference($packageName)
+{
+if (!isset(self::$installed['versions'][$packageName])) {
+throw new \OutOfBoundsException('Package "' . $packageName . '" is not installed');
+}
+
+if (!isset(self::$installed['versions'][$packageName]['reference'])) {
+return null;
+}
+
+return self::$installed['versions'][$packageName]['reference'];
+}
+
+
+
+
+
+public static function getRootPackage()
+{
+return self::$installed['root'];
+}
+
+
+
+
+
+
+
+public static function getRawData()
+{
+return self::$installed;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public static function reload($data)
+{
+self::$installed = $data;
+}
+}
